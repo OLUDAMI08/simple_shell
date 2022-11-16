ab6458ad41486a26593013201f17d974554e23bc
lawal damilare toheeb <lawaltd088@gmail.com>
2022-11-16 14:40:35 -0800

    complete

--- strtok.c
+++ strtok.c
@@ -3,3 +3,64 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "main.h"
+
+int main(int ac, char **argv){
+    char *prompt = "(shell) $ ";
+    char *lineptr = NULL, *lineptr_copy = NULL;
+    size_t n = 0;
+    ssize_t n_read;
+    const char *delim = " \n";
+    int num_tokens = 0;
+    char *token;
+    int i;
+
+    /* declaring void variables */
+    (void)ac;
+
+    while (1) {
+        printf("%s", prompt);
+        n_read = getline(&lineptr, &n, stdin);
+        if (nchars_read == -1){
+            printf("Exiting shell....\n");
+            return (-1);
+        }
+
+        lineptr_copy = malloc(sizeof(char) * n_read);
+        if (lineptr_copy== NULL){
+            perror("Error");
+            return (-1);
+        }
+        /* copy lineptr to lineptr_copy */
+        strcpy(lineptr_copy, lineptr);
+
+        token = strtok(lineptr, delim);
+
+        while (token != NULL){
+            num_tokens++;
+            token = strtok(NULL, delim);
+        }
+        num_tokens++;
+
+        argv = malloc(sizeof(char *) * num_tokens);
+
+        token = strtok(lineptr_copy, delim);
+
+        for (i = 0; token != NULL; i++){
+            argv[i] = malloc(sizeof(char) * strlen(token));
+            strcpy(argv[i], token);
+
+            token = strtok(NULL, delim);
+        }
+        argv[i] = NULL;
+
+
+    }
+
+
+    free(lineptr_copy);
+    free(lineptr);
+
+    return (0);
+}
+
