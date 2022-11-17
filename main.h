0e40919449770ba05ef00dce5a99476cc389bd7c
lawal toheeb damilare <lawaltd088@gmail.com>
2022-11-17 19:22:07 +0000

    header file

--- main.h
+++ main.h
@@ -11,5 +11,13 @@
 char *getpath(char *cmd);
 char **getcmd(char *lineptr_copy, const char *delim, ssize_t nread);
 void execmd(char **arg);
+int _strlen(char *str);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strdup(const char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strstr(char *haystack, char *needle);
+
 
 #endif
